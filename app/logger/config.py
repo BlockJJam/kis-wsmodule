92f8e7b196085f1ac93bc719f86ae72834430bf6
blockjjam <blockjjam99@gmail.com>
2022-06-23 17:46:43 +0900

    feat: config & setting update

---
+++ app/logger/config.py
@@ -0,0 +1,23 @@
+import logging
+
+def _formatter():
+    return logging.Formatter("\033[0;34m[%(asctime)s - %(name)s - %(levelname)s]\033[0m %(message)s")
+
+def _stream_handler():
+    handler = logging.StreamHandler()
+    handler.setFormatter(_formatter())
+    return handler
+
+def get_logger():
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    logger.addHandler(_stream_handler())
+    logger.propagate = 0
+    return logger
+
+
+logger = get_logger()
+
+'''
+쓰는 형테: logger.error(f'[CONTROLLER] {e}')
+'''
