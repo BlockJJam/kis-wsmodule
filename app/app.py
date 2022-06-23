7e5e34ee8b84e17d7546787117075a2f9c04366a
blockjjam <blockjjam99@gmail.com>
2022-06-23 13:51:33 +0900

    fixed: except .env in git repo

--- app/app.py
+++ app/app.py
@@ -1,7 +1,18 @@
 from pathlib import Path
+from fastapi import FastAPI
 import sys
 
 BASE_DIR = Path(__file__).resolve()
 sys.path.append(str(BASE_DIR))
 
-print('hello')
+def create_app() -> FastAPI:
+    app = FastAPI()
+    # app.include_router(api.live_router, prefix='/ws')
+
+    return app
+
+app = create_app()
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run('app:app', ws_ping_interval=5, ws_ping_timeout=5, log_level='debug')
