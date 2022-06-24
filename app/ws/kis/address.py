12ef146a63f8f755c0ed7d19f89a4b58bb4c0da8
blockjjam <blockjjam99@gmail.com>
2022-06-24 16:34:09 +0900

    fixed: type & address for KIS API

--- app/ws/kis/address.py
+++ app/ws/kis/address.py
@@ -7,12 +7,12 @@ class AddressBuilder:
     @staticmethod
     def get_address(urlType:Literal) -> str:
         # print( dp.get_env('KIS_DOMAIN')+ ':' + dp.get_env('KIS_PORT')+ dp.get_env(urlType.value[0]))
-        return dp.get_env('KIS_DOMAIN') + ':' + dp.get_env('KIS_PORT') + dp.get_env(urlType.value[0])
+        return dp.get_env('KIS_DOMAIN') + ':' + dp.get_env('KIS_PORT') + urlType.value
     
     @staticmethod
     def get_virtual_address(urlType:Literal) -> str:
-        print(dp.get_env('KIS_VIRTUAL_DOMAIN') + ':' + dp.get_env('KIS_VIRTUAL_PORT') + dp.get_env(urlType.value[0]))
-        return dp.get_env('KIS_VIRTUAL_DOMAIN') + ':' + dp.get_env('KIS_VIRTUAL_PORT') + dp.get_env(urlType.value[0])
+        print(dp.get_env('KIS_VIRTUAL_DOMAIN') + ':' + dp.get_env('KIS_VIRTUAL_PORT') + urlType.value)
+        return dp.get_env('KIS_VIRTUAL_DOMAIN') + ':' + dp.get_env('KIS_VIRTUAL_PORT') + urlType.value
 
 
         
--- app/ws/kis/types.py
+++ app/ws/kis/types.py
@@ -1,7 +1,12 @@
 from enum import Enum
 
 class UrlType(Enum):
-    HASHKEY='KIS_HASHKEY',
-    ACCESSKEY='KIS_ACCESSKEY',
-    ACCESSKEY_REMOVE='KIS_ACCESSKEY_REVOKE'
+    HASHKEY='/uapi/hashkey'
+    ACCESSKEY='/oauth2/tokenP'
+    ACCESSKEY_REMOVE='/oauth2/revokeP'
+    STOCK_MARKETPRICE='/uapi/domestic-stock/v1/quotations/inquire-price'
+    STOCK_EXECUTIONS='/uapi/domestic-stock/v1/quotations/inquire-ccnl'
+    STOCK_BY_DATE='/uapi/domestic-stock/v1/quotations/inquire-daily-price'
+    STOCK_ASKING_PRICE_EXPECTED_EXECUTIONS='/uapi/domestic-stock/v1/quotations/inquire-asking-price-exp-ccn'
+    ELK_MARKETPRICE='/uapi/domestic-stock/v1/quotations/inquire-elw-price'
 
